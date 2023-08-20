bb31e618ec42963e5d73c13af0d7343bbeef6b54
adzhumurat <dju0204@gmail.com>
2023-08-20 21:29:07 +0300

    Fix package name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='collab-research-toolkit',
+    name='colab_research_tools',
     version='0.1',
     packages=find_packages(),
     install_requires=[
