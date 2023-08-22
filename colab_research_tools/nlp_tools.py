ee0a0279bbc00aff114de099ea45a78496de6696
adzhumurat <dju0204@gmail.com>
2023-08-22 19:09:17 +0300

    Add fasttext tools

--- colab_research_tools/nlp_tools.py
+++ colab_research_tools/nlp_tools.py
@@ -157,3 +157,63 @@ def top_similar_tags(tag_id, candidates_array, candidates_embeds, top=10):
     print(tag_name)
     res = dict([(candidates_array[k], sims[k]) for k in top_similar_idx])
     return res
+
+#-------- FAST TEXT EMBEDDER -------- #
+def get_tokens(line):
+    tokens = line.split(' ')
+    return tokens
+
+def get_word_2_vec_vector(tokens, model):
+    return np.asarray([model[el] for el in tokens]).sum(axis=0)
+
+def get_normed_vector(raw_txt, model):
+    vec = get_word_2_vec_vector(get_tokens(raw_txt), model)
+    vec_norm = np.linalg.norm(vec)
+    vec /= vec_norm
+
+    return vec.astype(np.float32)
+
+def get_fasttext_corpus_vectors(df, model, synopsis_col: str = "synopsis"):
+    import tqdm
+
+    corpus_vectors = np.zeros(shape=(df.shape[0], 300)).astype(np.float32)
+    for raw, data in tqdm.tqdm(df.iterrows()):
+        vec = get_normed_vector(data[synopsis_col], model)
+        corpus_vectors[raw, :] = vec
+    corpus_vectors = corpus_vectors.astype(np.float32)
+    return corpus_vectors
+
+def eval_embeds(df, txt_col, npy_filename: str, root_data_dir: str, overwrite: bool = True):
+  if overwrite:
+    if os.path.exists(npy_filename):
+      os.remove(npy_filename)
+  if os.path.exists(npy_filename):
+    print('Loading from a dump')
+    corpus_numpy = np.load(npy_filename)
+  else:
+    # Warning: it takes 20 minutes to download the model!
+    print('Model loading...')
+    model = prepare_fasttext(root_data_dir)
+    print('Eval embeddings...')
+    corpus_numpy = get_fasttext_corpus_vectors(df, model, synopsis_col=txt_col)
+    print('Saving file %d rows to %s...' % (corpus_numpy.shape[0], npy_filename))
+    np.save(npy_filename, corpus_numpy)
+  return corpus_numpy
+
+def get_fasttext_vectors(candidates_list, model):
+    import tqdm
+
+    corpus_vectors = np.zeros(shape=(len(candidates_list), 300)).astype(np.float32)
+    for raw, data in tqdm.tqdm(enumerate(candidates_list)):
+        vec = get_normed_vector(data, model)
+        corpus_vectors[raw, :] = vec
+    corpus_vectors = corpus_vectors.astype(np.float32)
+    return corpus_vectors
+
+def concat_batches(batches_dir):
+  res = []
+  for f in os.listdir(batches_dir):
+    if '.npy' in f:
+      res.append(np.load(os.path.join(batches_dir, f)))
+  res = np.concatenate(res, axis=0)
+  return res
